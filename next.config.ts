7f0c3a16b60bf9605a3c0a83baeed22e78a8ae6d
mathisboche <bibi_76@outlook.fr>
2025-05-27 21:58:46 +0200

    ajout de redirect chess

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
       { source: '/daffe', destination: 'https://www.echecs.asso.fr/Reglements/Reglements2012/4%205%20DAFFE%201r%20degre.pdf', permanent: true },
       { source: '/fide', destination: 'https://ratings.fide.com/profile/651010240', permanent: true },
 
+      // CHESS.COM
+      { source: '/chess', destination: 'https://www.chess.com/member/mathis9748/stats', permanent: true },
+      { source: '/chesscom', destination: 'https://www.chess.com/member/mathis9748/stats', permanent: true },
+
       // INSTAGRAM
       { source: '/ig', destination: 'https://www.instagram.com/mathis.boche/', permanent: true },
       { source: '/instagram', destination: 'https://www.instagram.com/mathis.boche/', permanent: true },
